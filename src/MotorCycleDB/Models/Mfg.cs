e439238035856c92e1636426527cc9c8caa6a536
Mitch M <mitch0matthews@gmail.com>
2017-07-03 18:55:33 -0400

    Created Mfg and Model models and added 4 views

---
+++ src/MotorCycleDB/Models/Mfg.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MotorCycleDB.Models
+{
+    public class Mfg
+    {
+        public string Name { get; set; }
+        //public string Role { get; set; }
+    }
+}
+}
