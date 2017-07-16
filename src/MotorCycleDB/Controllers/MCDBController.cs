ce7bb06925bb9ca7b93a488d86f634c511fed6c0
Mitch M <mitch0matthews@gmail.com>
2017-07-15 21:57:40 -0400

    Got the motorcycle list and index views to display

---
+++ src/MotorCycleDB/Controllers/MCDBController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MotorCycleDB.Controllers
+{
+    public class MCDBController : Controller
+    {
+        // Directs the browser to the "Home or index" page
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // Directs the browser to the "Motorcycle List" page
+        public ActionResult MotorcycleList()
+        {
+            return View();
+        }
+    }
+}
