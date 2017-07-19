c13116dc1ef88328a688cc3d0fdc4a50bb703a91
Mitch M <mitch0matthews@gmail.com>
2017-07-19 10:47:32 -0400

    refinements on views

--- src/MotorCycleDB/Models/Motorcycle.cs
+++ src/MotorCycleDB/Models/Motorcycle.cs
@@ -20,28 +20,13 @@ namespace MotorCycleDB.Models
         public string Style { get; set; }
         public string Description { get; set; }
 
-        //public string GetDisplayBike()
-        //{
-        //    return ModelYear + " " + ModelId;
-        //}
+     
 
             // Add DB context class for communication with database
         public class MotorcycleDBContext : DbContext
         {
             public DbSet<Motorcycle> Motorcycles { get; set; }
         }
-
-        //public string BikeImage
-        //{
-        //    get
-        //    {
-        //        return SeriesTitle.Replace
-        //    }
-        //}
-
-        //public Engine Engine { get; set; }
-        //public Chassis Chassis { get; set; }
-        //public Mfg Mfg { get; set; }
 
     }
 }
